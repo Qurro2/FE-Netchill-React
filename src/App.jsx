5285ca397bf7979b274b8c0b0f36086a398bb187
Qurrrrr <kuriayun@gmail.com>
2024-03-08 15:42:39 +0700

    add react helmet

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { useState } from "react";
-import UserNoLogin from "./pages/UserNoLogin";
 import Router from "./routes/Router";
+import { Helmet } from "react-helmet";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
+      <Helmet>
+        <title>Netchill</title>
+        <link rel="icon" href="/n.svg" type="image/svg+xml" />
+      </Helmet>
       <main>
         <Router />
       </main>
